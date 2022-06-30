f372c165cc82d5f08daa7cb2c4bcb68897d873db
Diogo Amaral <dlamaral12@gmail.com>
2022-06-30 21:54:11 +0100

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Diogo Amaral on 30/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
